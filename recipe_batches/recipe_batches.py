59e4bcd9b1ccfcb5d9bc9a2ae54b176fccbc6496
Jacob McFaul <mcjakedude@gmail.com>
2019-06-05 15:25:59 -0700

    I'm not sure what i'm doing

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -3,12 +3,36 @@
 import math
 
 def recipe_batches(recipe, ingredients):
-  pass 
+  recipeItem = list(recipe.keys())
+  recipeValue = list(recipe.values())
+  ingredientsItem = list(ingredients.keys())
+  ingredientsQuantity = list(ingredients.values())
+  newValues = []
+  if len(recipeItem) > len(ingredientsItem):
+    batches = 0
+    return batches
+  else:
+    for i in range(0, len(recipeItem)-1):
+      if recipeValue[i] <= ingredientsQuantity[i]:
+        while ingredientsQuantity[i] >= recipeValue[i]:
+          x = ingredientsQuantity[i]/recipeValue[i]
+          newValues.append(x)
+      else:
+        batches = 0
+      
+
+  return batches
 
 
-if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
-  # your implementation with different inputs
-  recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
-  ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
-  print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+# if __name__ == '__main__':
+#   # Change the entries of these dictionaries to test 
+#   # your implementation with different inputs
+#   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+#   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+#   print("{batches} batches can be made from the available ingredients: {ingredients}.".format(batches=recipe_batches(recipe, ingredients), ingredients=ingredients))
+
+
+recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
+ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
+
+print(recipe_batches(recipe, ingredients))
